1c7883aee23ba52eabb363b8b477ded7647f817a
songhague <song2000s@naver.com>
2022-10-31 15:46:00 +0900

    [HA] 10주차

---
+++ Calender/class10/index.js
@@ -0,0 +1,9 @@
+const{createApp} = Vue;
+
+        createApp({
+            data(){
+                return{
+                    message : "helllow my name is song ha gyue"
+                }
+            }
+        }).mount("#app")/*뷰객체를 생성해서 div에다가 mount(안에 get elemnent By Id가 있음)*/
